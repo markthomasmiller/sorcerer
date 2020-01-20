c3a6ead20cb1a5c15bf9c76d9b3c42a877524afb
Mark Thomas Miller <mark@mayvalellc.com>
2020-01-20 14:41:21 -0500

    Rename to Sorcerer, add images

--- index.js
+++ index.js
@@ -1,31 +1,34 @@
+// Store references to page elements.
 const selector = document.getElementById("selector");
 const image = document.getElementById("image");
 
-const languages = [
-  { name: "JSX", url: "alchemy/images/jsx.png", active: true },
-  { name: "HTML", url: "alchemy/images/html.png" },
-  { name: "CSS", url: "alchemy/images/css.png" },
-  { name: "JSON", url: "alchemy/images/json.png" },
-  { name: "Ruby", url: "alchemy/images/ruby.png" }
-];
+// List each language to preview. Add a corresponding PNG in the "images" folder
+// with the same name, all lowercase. For instance, "JSX" is "jsx.png".
+const languages = ["JSX", "CSS", "JSON", "HTML", "Markdown"];
 
-languages.forEach((language, i) => {
+// Create buttons to show a preview of each language.
+languages.forEach((language, index) => {
   const button = document.createElement("button");
-  button.innerText = language.name;
-  button.setAttribute("data-count", i);
-  if (language.active) button.classList.add("active");
+  button.innerText = language;
+  button.setAttribute("data-index", index);
   selector.appendChild(button);
 });
 
+// Store the buttons in an array. We'll use this for "highlight".
+const buttons = Array.from(selector.getElementsByTagName("button"));
+
+// Add an active indicator to the first button.
+highlight(buttons[0]);
+
+// When a button is clicked, show its image.
 selector.addEventListener("click", e => {
   if (e.target.nodeName !== "BUTTON") return false;
-  const n = e.target.getAttribute("data-count");
-  image.src = languages[n].url;
+  const index = e.target.getAttribute("data-index");
+  image.src = `sorcerer/images/${languages[index].toLowerCase()}.png`;
   highlight(e.target);
 });
 
-const buttons = Array.from(selector.getElementsByTagName("button"));
-
+// Add an indicator class to a button if its language is being previewed.
 function highlight(element) {
   buttons.map(button => button.classList.remove("active"));
   element.classList.add("active");
---
+++ sorcerer/README.md
@@ -0,0 +1,16 @@
+# Sorcerer: a vibrant dark theme for VS Code
+
+![Sorcerer, a VS Code theme](images/jsx.png)
+
+Sorcerer is a vibrant dark theme for VS Code. It uses the same colors as [MageTools](https://mage.tools):
+
+- Pink: `#ff006a`
+- Blue: `#44dfff`
+- Green: `#aaed36`
+- Yellow: `#f5af19`
+- Gray: `#6e7d9a`
+- Dark Blue: `#0e141a`
+
+Special care was given to the appearance of JS/JSX, HTML, CSS, JSON, and Markdown. See more images on Sorcerer's [homepage](https://sorcerer.mage.tools).
+
+Made by [Mark Thomas Miller](https://zeph.co) ([Twitter](https://twitter.com/mayvalemark)).
